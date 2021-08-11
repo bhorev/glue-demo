232c0530d60d16afc78913591f43ecfe118ff260
bhorev <65091963+bhorev@users.noreply.github.com>
2021-08-12 02:28:11 +0300

    Update demo-job.py

--- demo-job.py
+++ demo-job.py
@@ -22,14 +22,24 @@ args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Transformation - relationalize (and orc)
+# Load
 datasource0 = glueContext.create_dynamic_frame.from_catalog(database = glue_source_database, table_name = glue_source_table, transformation_ctx = "datasource0")
-dfc = Relationalize.apply(frame = datasource0, staging_path = glue_temp_storage, name = dfc_root_table_name, transformation_ctx = "dfc")
+
+# Resolve choices for type conflicts
+Transform0 = datasource0.resolveChoice(choice = "cast:string")
+# Drop fields not needed
+#Transform0 = DropFields.apply(frame = datasource0, paths = ["ExifTool", "cuckoo_data"], transformation_ctx = "Transform0")
+
+# Transformation - relationalize
+dfc = Relationalize.apply(frame = Transform0, staging_path = glue_temp_storage, name = dfc_root_table_name, transformation_ctx = "dfc")
 mydata = dfc.select(dfc_root_table_name)
 
+#mydata.printSchema()
+#mydata.show(5)
+
 # Repartition and to parquet
-dailyData = mydata.repartition(1)
-parquetoutput = glueContext.write_dynamic_frame.from_options(frame = dailyData, connection_type = "s3", connection_options = {"path": glue_repartitioned_parquet_output_s3_path}, format = "parquet", transformation_ctx = "parquetoutput")
+#dailyData = mydata.repartition(1)
+parquetoutput = glueContext.write_dynamic_frame.from_options(frame = mydata, connection_type = "s3", connection_options = {"path": glue_repartitioned_parquet_output_s3_path, "partitionKeys": ["week"]}, format = "parquet", transformation_ctx = "parquetoutput")
 
 # Close
 job.commit()
